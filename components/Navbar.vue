3344a4e4bc49e27f4739455241ad750afcde4d02
signofcommunication <jhvonsky@gmail.com>
2024-10-14 22:59:19 +0700

    [components]:Added navbar [assets] [config-files]: initialize tailwind

---
+++ components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav>
+    <div class="gaws-logo">
+      <img src="/assets/gaws-logo.png" alt="GAWS LOGO" />
+    </div>
+    <ul>
+      <li>
+        <a href="/services">Services</a>
+      </li>
+      <li>
+        <a href="/about-us">About Us</a>
+      </li>
+      <li>
+        <a href="/career">Career</a>
+      </li>
+      <li>
+        <a href="/blog">Blog</a>
+      </li>
+      <li>
+        <a href="/contact">Contact</a>
+      </li>
+    </ul>
+  </nav>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+</template>
+
+<style scoped>
+nav {
+    background-color: #070051;
+    display: flex;
+    justify-content: space-between;
+    color: #fff;
+}
+
+nav ul {
+    display: flex;
+    justify-content: space-between;
+}
+
+nav ul li a {
+    color: #fff;
+}
+</style>
