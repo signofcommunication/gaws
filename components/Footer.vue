b4bf18dfae664e1fd048f53b40094e9e0bb053f5
signofcommunication <jhvonsky@gmail.com>
2024-10-15 22:30:42 +0700

    Feat: Add footer

---
+++ components/Footer.vue
@@ -0,0 +1,104 @@
+<template>
+  <footer class="bg-white dark:bg-gray-900">
+    <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+      <div class="md:flex md:justify-around">
+        <div class="mb-6 md:mb-0">
+          <a href="/" class="flex items-center mb-4">
+            <img src="/assets/gaws-logo.png" class="h-28" alt="Gaws Logo" />
+          </a>
+          <span class="text-white text-xl font-semibold">
+            Let's start your <br />
+            Digital Journey with us
+          </span>
+          <p class="text-white mt-4">
+            Gaws is the best partner for you to reach your <br />
+            digital market with maximum potential
+          </p>
+        </div>
+        <div class="grid grid-cols-2 gap-6 sm:grid-cols-3">
+          <div>
+            <h2
+              class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white"
+            >
+              Services
+            </h2>
+            <ul class="text-gray-500 dark:text-gray-400 font-medium">
+              <li class="mb-4">
+                <a href="https://flowbite.com/" class="hover:underline"
+                  >Web Development</a
+                >
+              </li>
+              <li class="mb-4">
+                <a href="https://tailwindcss.com/" class="hover:underline"
+                  >RFID Solution</a
+                >
+              </li>
+              <li class="mb-4">
+                <a href="https://tailwindcss.com/" class="hover:underline"
+                  >Desktop Application</a
+                >
+              </li>
+              <li class="mb-4">
+                <a href="https://tailwindcss.com/" class="hover:underline"
+                  >Embedded Systems</a
+                >
+              </li>
+              <li class="mb-4">
+                <a href="https://tailwindcss.com/" class="hover:underline"
+                  >n-Tier(Web Apps)</a
+                >
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2
+              class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white"
+            >
+              About
+            </h2>
+            <ul class="text-gray-500 dark:text-gray-400 font-medium">
+              <li class="mb-4">
+                <a
+                  href="https://github.com/themesberg/flowbite"
+                  class="hover:underline "
+                  >Our Story</a
+                >
+              </li>
+              <li class="mb-4">
+                <a href="https://discord.gg/4eeurUVvTy" class="hover:underline"
+                  >Visi & Misi Perusahaan</a
+                >
+              </li>
+              <li class="mb-4">
+                <a href="https://discord.gg/4eeurUVvTy" class="hover:underline"
+                  >Profil Manajemen</a
+                >
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2
+              class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white"
+            >
+              Contact Us
+            </h2>
+            <ul class="text-gray-500 dark:text-gray-400 font-medium">
+              <li class="mb-4">
+                <a href="#" class="hover:underline">(021) 42888951</a>
+              </li>
+              <li>
+                <a href="#" class="hover:underline">gaws-it@gaws-it.com</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+    background-color: #070051;
+}
+</style>
