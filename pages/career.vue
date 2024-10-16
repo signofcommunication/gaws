73fa49e718ddae8b384ade366a93f96f70aba073
signofcommunication <jhvonsky@gmail.com>
2024-10-16 14:13:31 +0700

    Feat: Header career

---
+++ pages/career.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="max-w-screen mx-auto">
+    <header class="w-full h-screen career-bg bg-no-repeat bg-cover bg-center">
+      <div
+        class="container flex flex-col items-center justify-center text-white mx-auto h-full"
+      >
+        <h1 class="text-2xl">Join Our Growing Team!</h1>
+        <h1 class="text-4xl text-center my-4">
+          We are looking for hardworking, passionate <br />
+          and inquisitive people.
+        </h1>
+        <button
+          type="button"
+          class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          View Opening Positon
+        </button>
+      </div>
+    </header>
+  </div>
+</template>
+
+<style scoped>
+.career-bg {
+    background-image: url('/assets/career.png');
+}
+</style>
