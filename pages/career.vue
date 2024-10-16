20dc657a6b65ae9feb3abbaaa900a9b2d22866b0
signofcommunication <jhvonsky@gmail.com>
2024-10-16 15:00:27 +0700

    Feat: slicing career page

--- pages/career.vue
+++ pages/career.vue
@@ -17,6 +17,18 @@
         </button>
       </div>
     </header>
+    <section class="py-8">
+      <div class="header flex flex-col items-center justify-center text-center">
+        <h1 class="font-bold text-4xl">Opening Position</h1>
+        <p class="max-w-6xl my-6">
+          We strive to create rockstar teams instead of rockstar individuals. If
+          you are a team player who always eager to learn new things or create
+          valuable application and make innovation, then maybe you are the right
+          person/talent for our team. Join us immediately!
+        </p>
+      </div>
+      <JobListing />
+    </section>
   </div>
 </template>
 
