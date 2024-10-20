9cbd8eb7c24f3c5d71e9859dd22f6d4d0fca6a78
signofcommunication <jhvonsky@gmail.com>
2024-10-20 09:57:50 +0700

    fix: Fix to tag to href in navbar component

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,7 +5,7 @@
     <div
       class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
     >
-      <a to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+      <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
         <img src="/assets/gaws-logo.png" class="h-26" alt="Gaws Logo" />
       </a>
       <button
