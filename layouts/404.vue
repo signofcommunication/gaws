502b5ea31bda82a6cbb40ad25226944b72a3f580
signofcommunication <jhvonsky@gmail.com>
2024-10-23 13:25:22 +0700

    Feat: add blog-detail

---
+++ layouts/404.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    Gada apa apa disini
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/blog-detail.vue
@@ -0,0 +1,27 @@
+<template>
+  <article
+    class="max-w-2xl px-6 py-24 mx-auto space-y-12 dark:bg-gray-100 dark:text-gray-900"
+  >
+    <div class="w-full mx-auto space-y-4 text-center">
+      <h1 class="text-4xl font-bold leading-tight md:text-5xl">
+        Interdum et malesuada fames ac ante ipsum primis in faucibus?
+      </h1>
+      <p class="text-sm dark:text-gray-600">
+        by
+        <a
+          rel="noopener noreferrer"
+          href="#"
+          target="_blank"
+          class="underline dark:text-violet-600"
+        >
+          <span itemprop="name">Jose Ryu</span>
+        </a>
+        on
+        <time datetime="2021-02-12 15:34:18-0200">Feb 12th 2021</time>
+      </p>
+    </div>
+    <div class="dark:text-gray-800">
+      <p>Insert the actual text content here...</p>
+    </div>
+  </article>
+</template>
