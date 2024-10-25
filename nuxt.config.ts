084af17e06bdb8b6ecf1b452b31df7f126d84f89
signofcommunication <jhvonsky@gmail.com>
2024-10-25 21:15:27 +0700

    Chore: update nuxt config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   },
 
   compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: ["~/assets/css/main.css"],
 
   postcss: {
