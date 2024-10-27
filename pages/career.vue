83d508352fd045c9b5ecd337d75747b47dad52f0
Jhvonsky <70561984+signofcommunication@users.noreply.github.com>
2024-10-27 20:42:25 +0700

    Feat: add animation onclick on career page

--- pages/career.vue
+++ pages/career.vue
@@ -7,22 +7,22 @@
       <div
         class="container flex flex-col items-center justify-center text-white mx-auto h-full px-4 text-center relative"
       >
-        <h1 class="text-xl sm:text-2xl md:text-3xl z-50">
+        <h1 class="text-xl sm:text-2xl md:text-3xl z-20">
           Join Our Growing Team!
         </h1>
         <h1
-          class="text-3xl sm:text-3xl md:text-4xl lg:text-5xl my-4 font-bold z-50"
+          class="text-3xl sm:text-3xl md:text-4xl lg:text-5xl my-4 font-bold z-20"
         >
           We are looking for hardworking, passionate
           <br class="hidden sm:block" />
           and inquisitive people.
         </h1>
-        <a
-          href="#job-listing"
-          class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 z-50 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-4 py-2.5 sm:px-5 sm:py-3 me-2 mb-2"
+        <button
+          class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 z-20 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-4 py-2.5 sm:px-5 sm:py-3 me-2 mb-2"
+          @click="scrollToSelection('#job-listing')"
         >
           View Opening Positions
-        </a>
+        </button>
       </div>
     </header>
 
@@ -52,3 +52,18 @@
   z-index: 1; /* Make sure the overlay is behind the text */
 }
 </style>
+
+<script setup>
+import gsap from 'gsap';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+
+gsap.registerPlugin(ScrollToPlugin);
+
+const scrollToSelection = (selector) => {
+  gsap.to(window, {
+    duration: 1,
+    scrollTo: selector,
+    ease: 'power1.inOut',
+  });
+};
+</script>
