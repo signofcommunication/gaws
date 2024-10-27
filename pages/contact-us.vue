490ac6a1ca8807ecd0cea578f94ec9bdb59335d7
Jhvonsky <70561984+signofcommunication@users.noreply.github.com>
2024-10-27 11:05:06 +0700

    Fix: Add ref for contact form

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -131,18 +131,19 @@
                 <h2 class="mb-4 text-2xl font-bold dark:text-white">
                   Ready to Get Started?
                 </h2>
-                <form id="contactForm">
+                <form @submit.prevent="handleSubmit" id="contactForm">
                   <div class="mb-6">
                     <div class="mx-0 mb-1 sm:mb-4">
                       <div class="mx-0 mb-1 sm:mb-4">
                         <label
                           for="name"
                           class="pb-1 text-xs uppercase tracking-wider"
-                        ></label
-                        ><input
+                          >Name</label
+                        >
+                        <input
                           type="text"
                           id="name"
-                          autocomplete="given-name"
+                          v-model="formData.name"
                           placeholder="Your name"
                           class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
                           name="name"
@@ -152,11 +153,12 @@
                         <label
                           for="email"
                           class="pb-1 text-xs uppercase tracking-wider"
-                        ></label
-                        ><input
+                          >Email</label
+                        >
+                        <input
                           type="email"
                           id="email"
-                          autocomplete="email"
+                          v-model="formData.email"
                           placeholder="Your email address"
                           class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
                           name="email"
@@ -167,10 +169,11 @@
                       <label
                         for="textarea"
                         class="pb-1 text-xs uppercase tracking-wider"
-                      ></label
-                      ><textarea
+                        >Message</label
+                      >
+                      <textarea
                         id="textarea"
-                        name="textarea"
+                        v-model="formData.message"
                         cols="30"
                         rows="5"
                         placeholder="Write your message..."
@@ -227,3 +230,14 @@
     </div>
   </div>
 </template>
+
+<script setup>
+  import { ref } from 'vue';
+
+// Define the form data
+const formData = ref({
+  name: '',
+  email: '',
+  message: ''
+});
+</script>
