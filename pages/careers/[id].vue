57c8e043eca07e1cae17fe931d2c20dcb3bf7514
Jhvonsky <70561984+signofcommunication@users.noreply.github.com>
2024-10-29 08:46:58 +0700

    Feat: route for individual job

---
+++ pages/careers/[id].vue
@@ -0,0 +1,11 @@
+<script setup>
+import { useRoute } from 'vue-router';
+import { ref, onMounted } from 'vue';
+
+const job = ref({})
+    const route = useRoute()
+</script>
+
+<template>
+  <div>Test</div>
+</template>
