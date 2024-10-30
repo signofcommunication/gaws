d5a2d466cd31bf54fb014860461c070027171849
Jhvonsky <70561984+signofcommunication@users.noreply.github.com>
2024-10-30 11:22:13 +0700

    Style: update footer justify

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="bg-white dark:bg-gray-900">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-      <div class="md:flex md:justify-around">
+      <div class="md:flex md:justify-between">
         <div class="mb-6 md:mb-0">
           <a href="/" class="flex items-center mb-4">
             <img src="/assets/gaws-logo.png" class="h-28" alt="Gaws Logo" />
