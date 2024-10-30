ec365ae3b005a3e63a49385d69b960aaee1ad362
Jhvonsky <70561984+signofcommunication@users.noreply.github.com>
2024-10-30 11:33:35 +0700

    style: update justify for footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,8 @@
 <template>
   <footer class="bg-white dark:bg-gray-900">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-      <div class="md:flex md:justify-between">
+      <div class="md:flex md:justify-between items-center">
+        <!-- Left Section with Logo and Text -->
         <div class="mb-6 md:mb-0">
           <a href="/" class="flex items-center mb-4">
             <img src="/assets/gaws-logo.png" class="h-28" alt="Gaws Logo" />
@@ -15,7 +16,9 @@
             digital market with maximum potential
           </p>
         </div>
-        <div class="grid grid-cols-2 gap-6 sm:grid-cols-3">
+
+        <!-- Right Section with Links -->
+        <div class="grid grid-cols-2 gap-8 sm:grid-cols-2">
           <div>
             <h2
               class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white"
@@ -26,18 +29,13 @@
               <li class="mb-4">
                 <a
                   href="https://github.com/themesberg/flowbite"
-                  class="hover:underline "
+                  class="hover:underline"
                   >Our Story</a
                 >
               </li>
               <li class="mb-4">
                 <a href="https://discord.gg/4eeurUVvTy" class="hover:underline"
                   >Visi & Misi Perusahaan</a
-                >
-              </li>
-              <li class="mb-4">
-                <a href="https://discord.gg/4eeurUVvTy" class="hover:underline"
-                  >Profil Manajemen</a
                 >
               </li>
             </ul>
@@ -65,6 +63,6 @@
 
 <style scoped>
 footer {
-    background-color: #070051;
+  background-color: #070051;
 }
 </style>
