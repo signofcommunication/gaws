6baa386760e76c25ec1b49fcc9baa343f42b06a5
signofcommunication <jhvonsky@gmail.com>
2024-11-05 21:14:58 +0700

    Fix: add location to footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -18,36 +18,69 @@
         </div>
 
         <!-- Right Section with Links -->
-        <div class="grid grid-cols-2 gap-8 sm:grid-cols-2">
-          <div>
-            <h2
-              class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white"
-            >
+        <div class="flex flex-col sm:flex-row sm:flex-wrap gap-8">
+          <!-- About Section -->
+          <div class="flex-1 min-w-[200px]">
+            <h2 class="mb-4 font-semibold uppercase tracking-wide text-white">
               About
             </h2>
-            <ul class="text-gray-500 dark:text-gray-400 font-medium">
-              <li class="mb-4">
-                <a href="/about" class="hover:underline">Our Story</a>
+            <ul class="text-white space-y-2 font-light">
+              <li>
+                <a
+                  href="/about"
+                  class="hover:underline transition hover:text-underline"
+                  >Our Story</a
+                >
               </li>
-              <li class="mb-4">
-                <a href="/about/#vision-mission" class="hover:underline"
-                  >Visi & Misi Perusahaan</a
+              <li>
+                <a
+                  href="/about/#vision-mission"
+                  class="hover:underline transition hover:text-underline"
+                  >Vision & Mission</a
                 >
               </li>
             </ul>
           </div>
-          <div>
-            <h2
-              class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white"
-            >
+
+          <!-- Contact Us Section -->
+          <div class="flex-1 min-w-[200px]">
+            <h2 class="mb-4 font-semibold text-white uppercase tracking-wide">
               Contact Us
             </h2>
-            <ul class="text-gray-500 dark:text-gray-400 font-medium">
-              <li class="mb-4">
-                <a href="#" class="hover:underline">(021) 42888951</a>
+            <ul class="text-white space-y-2 font-light">
+              <li>
+                <a
+                  href="tel:+622142888951"
+                  class="hover:underline transition hover:text-underline"
+                  >(021) 4288 8951</a
+                >
               </li>
               <li>
-                <a href="#" class="hover:underline">gaws-it@gaws-it.com</a>
+                <a
+                  href="mailto:gaws-it@gaws-it.com"
+                  class="hover:underline transition hover:text-underline"
+                  >gaws-it@gaws-it.com</a
+                >
+              </li>
+            </ul>
+          </div>
+
+          <!-- Our Location Section -->
+          <div class="flex-1 min-w-[200px]">
+            <h2 class="mb-4 text-white font-semibold uppercase tracking-wide">
+              Our Location
+            </h2>
+            <ul class="text-white space-y-2 font-light">
+              <li>
+                <a
+                  href="https://maps.app.goo.gl/Ap75EXRJUPoPD6xE6"
+                  target="_blank"
+                  class="hover:underline transition hover:text-underline"
+                >
+                  ITC Cemp. Mas, Komplek Blok N-10, Jl. Letjen Suprapto,<br />
+                  RW.8, Sumur Batu, Kec. Kemayoran,<br />
+                  DKI Jakarta, Jakarta 10640
+                </a>
               </li>
             </ul>
           </div>
